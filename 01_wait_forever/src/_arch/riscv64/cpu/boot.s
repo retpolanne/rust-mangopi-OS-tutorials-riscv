7d58c9be5c2f02475d462312a55cdd1904fa20ac
Anne Macedo <retpolanne@posteo.net>
2023-11-01 09:19:12 -0300

    Adds support to RISC-V on the first tutorial
    This patch adds mangopi bsp support, pointing to the correct toolchain
    and changes the low-level code to risc-v.

--- 01_wait_forever/src/_arch/riscv64/cpu/boot.s
+++ 01_wait_forever/src/_arch/riscv64/cpu/boot.s
@@ -13,8 +13,8 @@
 _start:
 	// Infinitely wait for events (aka "park the core").
 .L_parking_loop:
-	wfe
-	b	.L_parking_loop
+	wfi
+	j	.L_parking_loop
 
 .size	_start, . - _start
 .type	_start, function
